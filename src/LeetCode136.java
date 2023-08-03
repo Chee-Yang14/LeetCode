38b1557ac5e62da0dd6c5dd4e34f99d2338f7d2d
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2023-08-02 21:39:34 -0500

    r

---
+++ src/LeetCode136.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class LeetCode136 {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int x : nums) {
+            ans ^= x;
+        }
+        return ans;
+    }
+}
