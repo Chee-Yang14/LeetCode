971df74c845efa7bcd4e71546dc471c68ae2c182
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2023-08-03 22:24:50 -0500

    learned new way to memorization

--- src/LeetCode70.java
+++ src/LeetCode70.java
@@ -1,13 +1,20 @@
 public class LeetCode70 {
     public static void main(String[] args) {
         LeetCode70 test = new LeetCode70();
-        System.out.println(test.climbStairs(43));
+        System.out.println(test.climbStairs(5));
     }
 
     public int climbStairs(int n) {
+        if (n <= 1)
+            return 1;
 
-        if (n <= 2)
-            return n;
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        int[] track = new int[n + 1];
+        track[1] = 1;
+        track[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            track[i] = track[i - 1] + track[i - 2];
+        }
+        return track[n];
     }
 }
